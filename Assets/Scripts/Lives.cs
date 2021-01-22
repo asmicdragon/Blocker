efb30bd8230fb672cf4c8c126afcfc1bbab80d50
asmicdragon <kieran.magri.a101895@mcast.edu.mt>
2021-01-22 14:54:34 +0100

    Added Heart sprites
    - Added sprites for lives

---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lives : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    void ChangeSprite(){
+        if(GameManager.gameManager.lives == 3){
+            spriteRenderer.sprite = hearts[3]; 
+        } else if(GameManager.gameManager.lives == 2){
+            spriteRenderer.sprite = hearts[2]; 
+        }else if(GameManager.gameManager.lives == 1){
+            spriteRenderer.sprite = hearts[1]; 
+        }else if(GameManager.gameManager.lives == 0){
+            spriteRenderer.sprite = hearts[0]; 
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeSprite();
+    }
+}
