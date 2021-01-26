eb627f7f9642ac65579002b1062b30a5d259d835
asmicdragon <kieran.magri.a101895@mcast.edu.mt>
2021-01-26 19:14:12 +0100

    Fixing Particle System
    - Working arounda  fix for getting the particle system to work

---
+++ Assets/ParticleSystemController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemController : MonoBehaviour
+{
+
+    ParticleSystem system
+    {
+        get
+        {
+            if (_CachedSystem == null)
+                _CachedSystem = GetComponent<ParticleSystem>();
+            return _CachedSystem;
+        }
+    }
+    private ParticleSystem _CachedSystem;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(system){
+            if(StartingBlock.CurrentBlock == null){
+            system.Play();
+            system.Stop();
+            
+            }
+        }
+    }
+
+}
